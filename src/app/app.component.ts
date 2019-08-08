910a01ae2a26368335a9c03fd5a59d16d63fa343
Douglas Schwingel <dgls.sch@gmail.com>
2019-08-08 11:33:23 -0300

    Commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'AluraPic';
+  photos = [
+    {
+      description: 'Environment',
+      url: 'https://www.awarenessdays.com/wp-content/uploads/2018/07/iStock-473454180-1024x767.jpg'
+    },
+    {
+      description: 'Environment',
+      url: 'http://www-file.huawei.com/-/media/corporate/images/sustainability/2018/sustainability_environment.jpg'
+    },
+    {
+      description: 'Environment',
+      url: 'https://www.awarenessdays.com/wp-content/uploads/2018/07/iStock-473454180-1024x767.jpg'
+    },
+    {
+      description: 'Environment',
+      url: 'http://www-file.huawei.com/-/media/corporate/images/sustainability/2018/sustainability_environment.jpg'
+    }
+  ];
 }
